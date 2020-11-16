69407ac50584534faa27c8f89bcfbe2579f93bad
Max <maxim.macari.dam@gmail.com>
2020-11-16 15:30:29 +0100

    Initial Commit

---
+++ Animaiton/AnimaitonApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimaitonApp.swift
+//  Animaiton
+//
+//  Created by Maxim Macari on 16/11/20.
+//
+
+import SwiftUI
+
+@main
+struct AnimaitonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
